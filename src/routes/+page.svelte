e56267f4a0bbbe1d0e59929642e3502a88ffb2b9
krleitch <krleitch.ca@gmail.com>
2023-02-17 19:13:15 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import init, { greet } from 'wasm-synth';
+
+	onMount(async () => {
+		init().then((raw) => {
+			console.log('init wasm-pack', raw.memory);
+			greet();
+		});
+	});
+</script>
+
+<h1 class="text-sky-400">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import wasmPack from 'vite-plugin-wasm-pack';
+
+export default defineConfig({
+	plugins: [sveltekit(), wasmPack('./wasm-synth')],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
